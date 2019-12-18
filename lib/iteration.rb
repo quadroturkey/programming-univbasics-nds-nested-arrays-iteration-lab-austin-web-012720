bd9149adef8de7260849f5d45310a77372603937
Reese M Bowman <reesemb1@gmail.com>
2019-12-18 22:02:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,20 +1,45 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  row_index = 0
+  aos = []
+  while row_index < src.count do 
+    element_index_1 = 0
+    element_index_2 = 1
+    while element_index_1 < src[row_index].count do 
+      aos.push("I love " + src[row_index][element_index_1] + " and " + src[row_index][element_index_2] + " on my pizza")
+      element_index_1 += 2
+      element_index_2 += 2 
+    end
+    row_index += 1 
+  end
+  return aos
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+larger_values = []
+row_index = 0
+  while row_index < src.count do
+    larger_values << src[row_index].sort[1]
+    row_index += 1 
+  end
+  return larger_values
 end
 
+
 def total_even_pairs(src)
+even_nums = []
+row_index = 0 
+while row_index < src.count do
+  element_index = 0 
+  while element_index < src[row_index].count do 
+    if src[row_index][element_index] % 2 == 0 
+      even_nums << src[row_index][element_index]
+      element_index +=1
+    end
+    element_index +=1
+    end
+  end
+return even_nums
+end
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -22,4 +47,4 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-end
+
